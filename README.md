5a2691faecd2a75918cfb0cac4b000c7a65bba35
reidspencer <reid.spencer@yoppworks.com>
2022-06-07 18:38:25 -0400

    Initial fake functionality that builds & tests correctly.

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # improving-app-ui-gateway
 The User Interface Gateway for the improving.app implemented as a Kalix Action API
+
+# Overview
+This gateway provides the API that the user interface uses to communicate with 
+the back end of the application. One of its primary purposes is to encapsulate
+the back end services so that the user interface code is isolated from changes
+in the services or their APIs. All such translations occur within this UI 
+Gateway API. 
---
+++ src/main/scala/customer/api/UIGatewayServiceAction.scala
@@ -0,0 +1,22 @@
+package customer.api
+
+import kalix.scalasdk.action.Action
+import kalix.scalasdk.action.ActionCreationContext
+
+// This class was initially generated based on the .proto definition by Kalix tooling.
+//
+// As long as this file exists it will not be overwritten: you can maintain it yourself,
+// or delete it so it is regenerated as needed.
+
+class UIGatewayServiceAction(creationContext: ActionCreationContext) extends AbstractUIGatewayServiceAction {
+
+  /** Get the logged in user's permitted roles */
+  override def getRoles(getRoles: GetRoles): Action.Effect[UserRoles] = {
+    if (getRoles.userEmail.isEmpty) {
+      effects.error("Unspecified email address")
+    } else {
+      effects.reply(UserRoles(Seq("User")))
+    }
+  }
+}
+
