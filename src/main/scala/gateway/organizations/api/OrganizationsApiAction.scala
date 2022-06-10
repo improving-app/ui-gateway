f1c29a03c1c9727e5df8f619ed1de86e48254088
alexweinstein <alex.weinstein@billieapp.io>
2022-06-10 12:14:08 -0400

    IA-27 & IA-28: establishOrganization route w/ form validation

--- src/main/scala/gateway/organizations/api/OrganizationsApiAction.scala
+++ src/main/scala/gateway/organizations/api/OrganizationsApiAction.scala
@@ -1,9 +1,9 @@
 package gateway.organizations.api
 
-import gateway.organizations.domain.{OrgType, OrganizationEstablished}
 import kalix.scalasdk.action.{Action, ActionCreationContext}
 
 import java.util.UUID
+import scala.util.matching.Regex
 
 // This class was initially generated based on the .proto definition by Kalix tooling.
 //
@@ -11,15 +11,39 @@ import java.util.UUID
 // or delete it so it is regenerated as needed.
 
 class OrganizationsApiAction(creationContext: ActionCreationContext) extends AbstractOrganizationsApiAction {
-  override def establishOrganization(establishOrganization: EstablishOrganizationDto): Action.Effect[OrganizationEstablishedDto] = {
-    if(establishOrganization.baseInfo.isEmpty) effects.error("You need to provide organization info to establish an organization")
-    else {
-      val domainResult = establishOrganization.parentOrg match {
-        case Some(_) => OrganizationEstablished(UUID.randomUUID().toString, OrgType.ORG_TYPE_SUB)
-        case None => OrganizationEstablished(UUID.randomUUID().toString, OrgType.ORG_TYPE_BASE)
+  override def establishOrganization(establishOrganization: EstablishOrganizationCommand): Action.Effect[OrganizationEstablishedEvent] = {
+    def checkParentAndReply: Action.Effect[OrganizationEstablishedEvent] ={
+      val result = establishOrganization.parentOrg match {
+        case Some(_) => OrganizationEstablishedEvent(UUID.randomUUID().toString, OrgType.ORG_TYPE_SUB)
+        case None => OrganizationEstablishedEvent(UUID.randomUUID().toString, OrgType.ORG_TYPE_BASE)
       }
 
-      effects.reply(OrganizationEstablishedDto.defaultInstance.copy(orgId = domainResult.orgId, `type` = domainResult.`type`.name))
+      effects.reply(result)
+    }
+
+    establishOrganization.baseInfo match {
+      case None => effects.error("No organization info provided - cannot establish organization")
+      case Some(OrganizationInfo(_, _, None, _, _, _)) =>
+        effects.error("No address provided in organization info - cannot establish organization")
+      case Some(OrganizationInfo(_, _, Some(Address(_, _, _, _, _, None, _)), _, _, _)) =>
+        effects.error("No postal code provided in organization info - cannot establish organization")
+      case Some(OrganizationInfo(_, _, Some(Address(_, _, _, _, _, Some(PostalCode(region, _)), _)), _, _, _)) =>
+        if(region.isUs) region.us match {
+            case None => effects.error("No postal code provided in organization info - cannot establish organization")
+            case Some(USPostalCode(uspc, _)) =>
+              val regex = new Regex("\\d{5}(-\\d{4})?")
+              if(regex.matches(uspc)) checkParentAndReply
+              else effects.error("US Postal Code provided has invalid format - cannot establish organization")
+          }
+        else if(region.isCa) region.ca match {
+            case None => effects.error("No postal code provided in organization info - cannot establish organization")
+            case Some(CAPostalCode(capc, _)) =>
+              val regex = new Regex("[A-Z]\\d[A-Z]\\d[A-Z]\\d")
+              if(regex.matches(capc)) checkParentAndReply
+              else effects.error("CA Postal Code provided has invalid format - cannot establish organization")
+          }
+        else effects.error("Postal Code region not valid - cannot establish organization")
+      case Some(_) => checkParentAndReply
     }
   }
 }
